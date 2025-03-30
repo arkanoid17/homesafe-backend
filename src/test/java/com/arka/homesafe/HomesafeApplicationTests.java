733bd913466fbb02724f3f9a3cf0513325f57189
Arka <mahata65@gmail.com>
2025-03-31 01:10:30 +0530

    first commit

---
+++ src/test/java/com/arka/homesafe/HomesafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arka.homesafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomesafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
